4d99684274439c69f13aaaf9fc901a6b23d0daee
yogeshmkt <yogesh@markelytics.com>
2023-01-04 12:36:36 +0530

    create employee class

---
+++ src/main/java/com/vnsp/model/Employee.java
@@ -0,0 +1,38 @@
+package com.vnsp.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Cascade;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Data
+@NoArgsConstructor
+public class Employee {
+@Id
+	private int empId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String department;
+	private String phone;
+	private String adharNo;
+	private String panNo;
+	private double salary;
+	
+	
+	 @OneToMany(cascade=CascadeType.ALL)
+	 private List<Address> address;
+	 
+}
